f9d84480e16603970486a3f75925a237810f1266
syllogismos <anilkaraka@live.in>
2017-07-10 07:00:18 +0530

    login and register pages

--- src/app/pages/create-account.ts
+++ src/app/pages/create-account.ts
@@ -1,14 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Http, Headers } from "@angular/http";
+import { Router } from "@angular/router";
+
+import "rxjs/add/operator/toPromise";
 
 @Component({
   selector: 'create-account',
   templateUrl: '../pages/create-account.html'
 })
 
-export class CreateAccountComponent {
+export class CreateAccountComponent implements OnInit {
 
-  constructor() {
+  constructor(
+    private http: Http,
+    private router: Router
+  ) {
   }
 
+  ngOnInit() {}
+
+  onRegClick(email: string, password: string) {
+    console.log(email);
+    console.log(password);
+    console.log('registering the above user');
+    this.http.post('/api/register', {
+      'username': email,
+      'email': email,
+      'password': password,
+      'password1': password
+    }).toPromise()
+      .then(response => {
+        console.log('register response');
+        console.log(response.json());
+        localStorage.setItem('user', JSON.stringify(response.json()));
+        this.router.navigate(['/pages/login'])
+      })
+      .catch(this.handleHttpError)
+  }
+
+  private handleHttpError(error: any): Promise<any> {
+    console.error('An error occurred', error);
+    return Promise.reject(error.message || error);
+  }
 }
 
--- src/app/pages/login.ts
+++ src/app/pages/login.ts
@@ -1,13 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Http, Headers } from "@angular/http";
+import { Router } from "@angular/router";
+
+import "rxjs/add/operator/toPromise";
 
 @Component({
   selector: 'login',
   templateUrl: '../pages/login.html'
 })
 
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
-  constructor() {
+  constructor(
+    private http: Http,
+    private router: Router
+  ) {
+  }
+
+  ngOnInit() {}
+
+  onLoginClick(email: string, password: string) {
+    console.log(email);
+    console.log(password);
+    console.log('Im stupid');
+
+    this.http.post('/api/login', {
+      'username': email,
+      'password': password
+    }).toPromise()
+      .then(response => {
+        console.log('right after login response')
+        console.log(response.json())
+        localStorage.setItem('user', JSON.stringify(response.json()))
+        // this.router.navigate(['dashboard'])
+      })
+      .catch(this.handleHttpError)
+  }
+
+  onRegisterClick(): void {
+    this.router.navigate(['/pages/create-account']);
+  }
+
+  onLogoutClick() {
+    console.log('clicked on logout');
+    this.http.post('/api/logout', {})
+      .toPromise()
+      .then(response => {
+        console.log('api/logout responded');
+        console.log(response);
+        localStorage.setItem('user', null);
+        // and then route to login page
+      })
+      .catch(this.handleHttpError)
+  }
+
+  private handleHttpError(error: any): Promise<any> {
+    console.error('An error occurred', error);
+    return Promise.reject(error.message || error);
   }
 
 }
