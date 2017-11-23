c51a8634468f83a26ab7e8bd6363d05ce0719b82
syllogismos <anilkaraka@live.in>
2017-11-23 06:16:45 +0530

    release process test

--- create_build.sh
+++ create_build.sh
@@ -6,3 +6,5 @@ npm run build
 zip -r s3_build/dist_s3_build.zip dist_build
 
 s3cmd put s3_build/dist_s3_build.zip s3://eschernode-dashboard-build --force
+
+ssh ubuntu@52.2.113.244 "cd dashboard; sh release_build.sh"
