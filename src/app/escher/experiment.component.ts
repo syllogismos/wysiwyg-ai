afdff8b321767861b20d17c84eed2ccee74ce28c
syllogismos <anilkaraka@live.in>
2018-01-22 04:24:44 +0530

    experiment page

--- src/app/escher/experiment.component.ts
+++ src/app/escher/experiment.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ColorsService } from "../services/colors";
 import { ExperimentService } from "./experiment.service";
+import { Router } from "@angular/router";
 
 declare var $: any;
 declare var _: any;
@@ -21,18 +22,13 @@ export class ExperimentComponent implements OnInit {
 
   constructor(
     private colorsService: ColorsService,
-    private experimentService: ExperimentService
+    private experimentService: ExperimentService,
+    private router: Router
   ) {
     const palettes = colorsService.getPalettes();
     const background = $('body').attr('data-background');
     this.colors = colorsService.getBootstrapColors();
     this.palette = palettes[background];
-    if (background == 'light') {
-      this.palette['borderColor'] = colorsService.darken(this.palette['borderColor'], 10);
-    }
-    if (background == 'primary') {
-      this.palette['borderColor'] = colorsService.darken(this.palette['borderColor'], 30);
-    }
    }
 
   ngOnInit() {
@@ -50,67 +46,7 @@ export class ExperimentComponent implements OnInit {
         }, 10);
         // $('#experiment-datatable').DataTable();
     })
-
-    $.get('/assets/json/stats.json', data => {
-      this.experimentLogs = data
-      var mean = _.filter(this.experimentLogs.EpRewMean, (item, index) => {
-        return index % 100 == 0;
-      });
-
-      var maxMean = _.filter(this.experimentLogs.EpRewMax, (item, index) => {
-        return index % 100 == 0;
-      })
-
-      var iteration = _.filter(_.range(this.experimentLogs.EpLenMean.length), (item, index) => {
-        return index % 100 == 0;
-      })
-
-      var pol_after = _.filter(this.experimentLogs.pol_ent_after, (item, index) => {
-        return index % 100 == 0;
-      })
-
-      var pol_before = _.filter(this.experimentLogs.pol_ent_before, (item, index) => {
-        return index % 100 == 0;
-      })
-
-      pol_before = _.map(pol_before, x => {
-        return x + _.random(0.5)
-      })
-      
-      this.lineChart1('#avg-return-line-chart', [mean, maxMean], iteration)
-      this.lineChart1('#max-kl-line-chart', [pol_before, pol_after], iteration)
-      // console.log(this.experimentLogs.EpLenMean)
-    }, 'json');
-
-    this.easyPieChart('.easy-pie-chart-primary-xs', this.colors['primary'], this.palette['borderColor'], 100);
-    this.easyPieChart('.easy-pie-chart-danger-xs', this.colors['danger'], this.palette['borderColor'], 100);    
   }
-
-  easyPieChart(element: string, barColor: string, trackColor: string, size: number): void {
-    $(element).easyPieChart({
-      barColor: barColor,
-      size: size,
-      trackColor: trackColor,
-      scaleColor: false,
-      animate: false,
-      linewidth: 5,
-      lineCap: 'square'
-    })
-  }
-
-  lineChart1(element: string, series: any, labels: any): void {
-    const data = {
-      labels: labels,
-      series: series
-    };
-    const options = {
-      fullWidth: true,
-      showArea: false
-    };
-    new Chartist.Line(element, data, options);
-  }
-
-
   getExperiments(): void {
     // var experiments;
     this.experimentService.getExperiments()
@@ -128,6 +64,14 @@ export class ExperimentComponent implements OnInit {
       })
       
     // console.log(this.experiments)
+  }
+
+  launchRlExperiment(): void {
+    this.router.navigate(['escher/rl'])
+  }
+
+  launchSupExperiment(): void {
+    this.router.navigate(['escher/supervised'])
   }
 
 }
