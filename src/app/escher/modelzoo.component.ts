28e5afe6aa1a8fc4295dc06d2cc7fc6f0d2d7771
syllogismos <anilkaraka@live.in>
2018-01-23 00:08:38 +0530

    model preview in model page

--- src/app/escher/modelzoo.component.ts
+++ src/app/escher/modelzoo.component.ts
@@ -1,22 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from "@angular/router";
 import { Http } from "@angular/http";
+import { EditorService } from 'app/escher/editor.service';
+import { EscherService } from 'app/escher/escher.service';
 
 declare const $: any;
 declare const _: any;
+declare const fabric: any;
 
 @Component({
   selector: 'app-modelzoo',
   templateUrl: './modelzoo.component.html',
-  styleUrls: ['./modelzoo.component.scss']
+  styleUrls: ['./modelzoo.component.scss'],
+  providers: [EditorService, EscherService]
 })
 export class ModelzooComponent implements OnInit {
 
   models: any;
+  canvas: any;
+  canvasWrapper: any;
+  currentModel: any;
 
   constructor(
     private router: Router,
-    private http: Http
+    private http: Http,
+    public editorService: EditorService
   ) { }
 
   ngOnInit() {
@@ -28,6 +36,9 @@ export class ModelzooComponent implements OnInit {
       $(this).tooltip('hide')
     })
 
+    // this.canvas = new fabric.Canvas('canvas')
+    // this.canvasWrapper = $('#canvasWrapper')
+    // this.editorService.init(this.canvas, this.canvasWrapper)
 
     this.models = []
 
@@ -58,7 +69,7 @@ export class ModelzooComponent implements OnInit {
           m.name = model.name
           m.hidden = network.length
           m._id = model._id
-          m.network = network
+          m.network = model.network
           m.createdAt = model.createdAt
           return m
         })
@@ -79,8 +90,26 @@ export class ModelzooComponent implements OnInit {
       })
   }
 
-  selectModel(network): void {
-    console.log("selected model", network)
+  selectModel(model): void {
+    console.log("selected model", model)
+    if (this.currentModel === undefined) {
+      this.currentModel = model      
+      console.log("initializing canvas editor service")
+      setTimeout(() => {
+        // little timeout for when you introduce canvas and canvas wrapper elements into html in the beginning
+        this.canvas = new fabric.Canvas('canvas')
+        this.canvasWrapper = $('#canvasWrapper')
+        this.editorService.init(this.canvas, this.canvasWrapper)
+      }, 100)
+      setTimeout(() => {
+        // slightly larger timeout for the editor service to get initialized so that the network can be loaded
+        // on to the editor after wards.
+        this.editorService.loadModel(model.network)
+      }, 1000)
+    } else {
+      this.currentModel = model
+      this.editorService.loadModel(model.network)
+    }
   }
 
 }
