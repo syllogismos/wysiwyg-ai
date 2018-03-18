09ccf37b17a72f295331a9c12ad3cd314503a79c
syllogismos <anilkaraka@live.in>
2018-03-18 13:11:11 +0530

    make it easy to checkout sample neural networks

--- src/app/dashboards/intro.component.ts
+++ src/app/dashboards/intro.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { EscherService } from 'app/escher/escher.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-intro',
   templateUrl: './intro.component.html',
-  styleUrls: ['./intro.component.scss']
+  styleUrls: ['./intro.component.scss'],
+  providers: [EscherService]
 })
 export class IntroComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private escherService: EscherService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    // console.log(this.escherService['unet'])
+  }
+
+  editModel(name): void {
+    // console.log()
+    localStorage.setItem('localFabricCanvas', this.escherService[name])
+    this.router.navigate(['/escher/console'])
+    // this.escherService
   }
 
 }
